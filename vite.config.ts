67d35290a05cee960873d4d761652d26a42afa3a
Rajesh Rao <raorajesh7700@gmail.com>
2026-07-10 17:19:20 +0530

    tailwind issue resolved

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       generatedRouteTree: "./src/routeTree.gen.ts",
     }),
     react(),
+    tailwindcss(),
     tsconfigPaths(),
   ],
   resolve: {
